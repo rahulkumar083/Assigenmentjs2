a98b53f6a2ba40957807616903eac211b20bd312
rahulkumar083 <136946107+rahulkumar083@users.noreply.github.com>
2024-09-02 18:25:10 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,51 @@
+
+//Question-1
+let A=2
+let B=5
+let C=A+B
+console.log(C)
+
+//Qustion-2
+let a=5;
+let b=3;
+
+let c=(a>b && a==b);
+console.log(c);
+
+//Qustion-3
+
+let A=9;
+let B=9;
+    
+    if (A % 10 == 0 && B % 10 ==0) {
+        console.log("true");
+    }
+    
+    else if ((A % 10 != 0 && B % 10 !== 0) {
+        console.log("false");
+    }
+    
+    else if (A % 10 == 0 || B % 10 === 0) {
+        console.log("true");
+    }
+   // Qustion-4
+let num=4567;
+let res=(num)/1000;
+
+console.log(Math.floor(res));
+ // Qustion-5
+
+let nu1 =4567;
+let ld =4567%10;
+
+console.log(` ${ld}`);
+//Qustion-6.
+let p = 36;
+let q = 6 ;
+
+console.log(p % q)
+//Qustion-6
+let m = 2;
+   let n = 3;
+
+   console.log(m * n)
